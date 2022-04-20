ab5e075348b03cd50fcfa10c6c996f43c77f5a9f
Olbrien <godexx@hotmail.com>
2022-04-20 05:05:21 +0100

    Tests purpose for ssl

--- inception/srcs/requirements/nginx/tools/startup.sh
+++ inception/srcs/requirements/nginx/tools/startup.sh
@@ -2,3 +2,7 @@
 # I substitute them here
 
 sed -i "s/__SCHOOL_USERNAME__/$SCHOOL_USERNAME/g" /etc/nginx/sites-enabled/default
+
+
+# Tests purpose, to delete
+sed -i 's/nginx/tisantos and skrillex/g' /var/www/html/index.nginx-debian.html
